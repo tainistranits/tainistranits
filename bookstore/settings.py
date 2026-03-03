f665b4ec1c7b73b60b98e7764f04ff5173c80f59
tainistranits <tainistranits@atomicmail.io>
2026-03-03 14:47:05 +0300

    Update settings.py

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2lldutt=1ag3*of@)b45=lm6bj!j!u4#laez%9+2sci2c7d&8)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["tainistranits-production.up.railway.app"]
 
